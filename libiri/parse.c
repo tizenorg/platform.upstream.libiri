3375c6befda12008e5eeb4e6b898b3ab38a47ac2
mo@ilaven.net <mo@ilaven.net@68b1170a-6346-0410-b79c-01ab32b2924b>
2008-11-19 14:34:53 +0000

    Initial import.
    git-svn-id: http://libiri.googlecode.com/svn/trunk@2 68b1170a-6346-0410-b79c-01ab32b2924b

---
+++ libiri/parse.c
@@ -0,0 +1,224 @@
+/*
+ * Copyright (c) 2005, 2008 Mo McRoberts.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The names of the author(s) of this software may not be used to endorse
+ * or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * AUTHORS OF THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "p_libiri.h"
+
+static inline const char *
+iri__copychar_decode(char **dest, const char *src)
+{
+	**dest = *src;
+	src++;
+	(*dest)++;
+	return src;
+}
+
+static inline char *
+iri__allocbuf(const char *src)
+{
+	/* Calculate the size of the buffer required to hold a decoded version of
+	 * src, including enough breathing space for null bytes.
+	 */
+	/* XXX: This is way too much; we need to actually count it */
+	return (char *) calloc(1, (strlen(src) * 4) + 16);
+}
+
+iri_t *
+iri_parse(const char *src)
+{
+	iri_t *p;
+	char *bufstart, *endp, *bufp;
+	const char *at, *colon, *slash;
+	
+	if(NULL == (p = (iri_t *) calloc(1, sizeof(iri_t))))
+	{
+		return NULL;
+	}
+	if(NULL == (bufstart = iri__allocbuf(src)))
+	{
+		free(p);
+		return NULL;
+	}
+	p->_private = bufp = bufstart;
+	at = strchr(src, '@');
+	slash = strchr(src, '/');
+	colon = strchr(src, ':');
+	if(slash && colon && slash < colon)
+	{
+		/* We can disregard the colon if a slash appears before it */
+		colon = NULL;
+	}
+	if(colon && !at)
+	{
+		/* Definitely a scheme */
+		p->scheme = bufp;
+		while(*src && *src != ':')
+		{
+			src = iri__copychar_decode(&bufp, src);
+		}
+		*bufp = 0;
+		bufp++;
+		src++;
+		/* src[0-1] SHOULD == '/' */
+		for(; *src == '/'; src++);
+	}
+	else if(colon && at && colon < at)
+	{
+		/* This could be scheme://user[:auth]@host or [scheme:]user[:auth]@host (urgh) */
+		if(colon[1] == '/')
+		{
+			p->scheme = bufp;
+			while(*src && *src != ':')
+			{
+				src = iri__copychar_decode(&bufp, src);
+			}
+			*bufp = 0;
+			bufp++;
+			src++;
+			/* src[0-1] SHOULD == '/' */
+			for(; *src == '/'; src++);
+			p->user = bufp;
+		}
+		else
+		{
+			p->scheme = bufp;
+		}
+		while(*src && *src != ':' && *src != '@')
+		{
+			src = iri__copychar_decode(&bufp, src);
+		}
+		*bufp = 0;
+		bufp++;
+		if(*src == ':')
+		{
+			/* Following auth data */
+			src++;
+			p->auth = bufp;
+			while(*src && *src != ':' && *src != '@')
+			{
+				src = iri__copychar_decode(&bufp, src);
+			}
+			*bufp = 0;
+			bufp++;
+			if(*src == ':')
+			{
+				src++;
+				/* It was actually scheme:user:auth@host */
+				p->user = p->auth;
+				p->auth = bufp;
+				while(*src && *src != '@')
+				{
+					src = iri__copychar_decode(&bufp, src);
+				}
+				*bufp = 0;
+				bufp++;
+			}
+		}
+		if(!*src)
+		{
+			/* No host part */
+			return p;
+		}
+		if(*src == '@')
+		{
+			src++;
+		}
+	}
+	else if(at)
+	{
+		/* user@host[/path...] */
+		p->user = bufp;
+		while(*src != '@')
+		{
+			src = iri__copychar_decode(&bufp, src);
+		}
+		*bufp = 0;
+		bufp++;
+		src++;
+	}
+	p->host = bufp;
+	while(*src && *src != ':' && *src != '/' && *src != '?' && *src != '#')
+	{
+		src = iri__copychar_decode(&bufp, src);
+	}
+	*bufp = 0;
+	bufp++;
+	if(*src == ':')
+	{
+		/* Port part */
+		src++;
+		endp = (char *) src;
+		p->port = strtol(src, &endp, 10);
+		src = endp;
+	}
+	if(*src == '/')
+	{
+		p->path = bufp; 
+		while(*src && *src != '?' && *src != '#')
+		{
+			src = iri__copychar_decode(&bufp, src);
+		}
+		*bufp = 0;
+		bufp++;
+	}
+	if(*src == '?')
+	{
+		p->query = bufp; 
+		src++;
+		while(*src && *src != '#')
+		{
+			src = iri__copychar_decode(&bufp, src);
+		}
+		*bufp = 0;
+		bufp++;
+	}
+	if(*src == '#')
+	{
+		p->anchor = bufp; 
+		while(*src)
+		{
+			src = iri__copychar_decode(&bufp, src);
+		}
+		*bufp = 0;
+		bufp++;
+	}
+	if(*src)
+	{
+		/* Still stuff left? It must be a path... of sorts */
+		p->path = bufp; 
+		while(*src && *src != '?' && *src != '#')
+		{
+			src = iri__copychar_decode(&bufp, src);
+		}
+		*bufp = 0;
+		bufp++;
+	}
+	return p;
+}
---
+++ tests/iridump.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2005, 2008 Mo McRoberts.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The names of the author(s) of this software may not be used to endorse
+ * or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * AUTHORS OF THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "iri.h"
+
+int
+main(int argc, char **argv)
+{
+	iri_t *iri;
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "Usage: %s IRI\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	iri = iri_parse(argv[1]);
+	printf("scheme: %s\n", iri->scheme);
+	printf("  user: %s\n", iri->user);
+	printf("  auth: %s\n", iri->auth);
+	printf("  host: %s\n", iri->host);
+	printf("  port: %d\n", iri->port);
+	printf("  path: %s\n", iri->path);
+	printf(" query: %s\n", iri->query);
+	printf("anchor: %s\n", iri->anchor);
+	return 0;
+}
